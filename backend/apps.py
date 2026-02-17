04df79c2c53031c5294b36b1b1bb2be250296c1b
Oleksandr <zadorozhniy3@icloud.com>
2026-02-17 15:08:11 +0200

    rename backend dir

--- backend/apps.py
+++ backend/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PingerAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pinger_app'
+    name = 'backend'
--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from pinger_app.views import ServiceViewSet, DashboardView
+from backend.views import ServiceViewSet, DashboardView
 
 
 router = DefaultRouter()
@@ -11,7 +11,7 @@ router.register(r'services', ServiceViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('pinger_app.urls')),
+    path('api/', include('backend.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', DashboardView.as_view(), name='dashboard'),
